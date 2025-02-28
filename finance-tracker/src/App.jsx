3584d71cd2947b1dd62d719757472a93b3a13e42
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2025-02-27 18:34:15 -0600

    Added ExpenseList and ExpenseItem components to be able to render the expenses properly

--- finance-tracker/src/App.jsx
+++ finance-tracker/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
 
 function App() {
   return (
@@ -20,6 +21,12 @@ function App() {
             </div>
             <div className='col-sm'>
                 <ExpenseTotal />
+            </div>
+        </div>
+        <h3 className='mt-3'>Expenses</h3>
+        <div className='row mt-3'>
+            <div className='col-sm'>
+                <ExpenseList />
             </div>
         </div>
     </div>
