0487744407a0250b8878d92f230617c180e017df
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2025-02-28 18:12:49 -0600

    Added an add expense form and rendered it on the page

--- finance-tracker/src/components/ExpenseItem.jsx
+++ finance-tracker/src/components/ExpenseItem.jsx
@@ -6,7 +6,13 @@ const ExpenseItem = (props) => {
         <li className='list-group-item d-flex justify-content-between align-items-center'>
             {props.name}
             <div>
-                <span className='badge badge-primary badge-pill mr-3' style={{color: 'black' }}>
+                <span className='badge badge-primary badge-pill mr-3'
+                style={{
+                    backgroundColor: 'blue',
+                    color: 'white',
+                    padding: '0.5em',
+                    borderRadius:  '0.5em'
+                }}>
                     ${props.cost}
                 </span>
                 <TiDelete size='1.5em'></TiDelete>
